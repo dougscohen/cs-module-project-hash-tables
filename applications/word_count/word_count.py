89e5a5cae85981883ed345a52265d0b7a6602fd8
Doug Cohen <dougscohen@gmail.com>
2020-07-08 17:33:34 -0400

    completed word count and test ran ok

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,32 @@
 def word_count(s):
-    # Your code here
+    # define unwanted characters to take out
+    unwanted_chars ='''":;,.-+=/\|[]{}()*^&'''
 
+    # ignore the case and make everything lower case
+    lower_case = s.lower()
+
+    # remove unwanted characters from the string
+    cleaned_string = ""
+    for char in lower_case:
+        if char not in unwanted_chars:
+            cleaned_string = cleaned_string + char
+
+    # split the words
+    word_list = cleaned_string.split()
+    
+    # Set empty dictionary for words (keys) and their counts (values)
+    counts = {}
+    
+    # iterate through word list
+    for word in word_list:
+        # if the word exists in counts, add one to the count
+        if word in counts:
+            counts[word] += 1
+        # otherwise, add the word as a key and make its value 1
+        else:
+            counts[word] = 1
+            
+    return counts
 
 
 if __name__ == "__main__":
