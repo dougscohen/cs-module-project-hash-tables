4928bbaa99d75842672c370cb5b891ed14995e6b
Doug Cohen <dougscohen@gmail.com>
2020-07-09 11:57:20 -0400

    completed expensive_seq and test ran ok

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,20 @@
-# Your code here
 
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
-
+    # if x is 0 or negative, return y + z
+    if x <= 0:
+        return y + z
+    
+    # otherwise
+    else:
+        # if the tuple (x, y, z) is not in the cache
+        if (x, y, z) not in cache:
+            # add it as a key
+            cache[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        
+        # return the key
+        return cache[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
